c2c43bd39078703ec7d22414eb562c0762df46fc
yonghoow <2073951b@student.tp.edu.sg>
2022-11-12 22:04:23 +0800

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-SECRET_KEY = config("SECRET_KEY")
+#from decouple import config
+#SECRET_KEY = config("SECRET_KEY")
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
